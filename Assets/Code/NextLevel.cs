c5479d4b91afd631a1b3a847201cd64efd76e1fd
Sabahat-Sami <ss14243@nyu.edu>
2022-09-15 15:55:43 -0400

    Added mirror to new level

---
+++ Assets/Code/NextLevel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public string levelName = "Level1";
+    private void OnTriggerEnter(Collider other){
+        if(other.tag == "Player"){
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
