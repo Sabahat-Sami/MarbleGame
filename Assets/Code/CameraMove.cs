2ef84987d7217a66ad14a7cf1e64626b3bff2553
Sabahat-Sami <70985974+Sabahat-Sami@users.noreply.github.com>
2022-09-15 17:56:37 -0400

    Added AI navigation and camera rotation

--- Assets/Code/CameraMove.cs
+++ Assets/Code/CameraMove.cs
@@ -6,6 +6,8 @@ public class CameraMove : MonoBehaviour
 {
     Transform playerTrans;
     Vector3 offSet;
+    float turnSpeed = 5;
+    bool wasSpacePressed = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,14 @@ public class CameraMove : MonoBehaviour
     void LateUpdate()
     {
         transform.position = playerTrans.position;
+        if(Input.GetKeyDown(KeyCode.Space)){
+            transform.Rotate(new Vector3(0, 180, 0));
+            wasSpacePressed = true;
+        }
+        if(Input.GetKeyUp(KeyCode.Space) && wasSpacePressed){
+            transform.Rotate(new Vector3(0, 180, 0));
+            wasSpacePressed = false;
+        }
     }
+
 }
